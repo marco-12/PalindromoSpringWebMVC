78a2432c9bd5ddb846e3c91fccbe8adc0677bba1
marco-12 <marco.simone95@hotmail.com>
2020-06-16 10:25:01 +0200

    Risolti alcuni errori

--- PalindromoSpringWebMVC/src/main/java/it/dstech/formazione/controller/ControlloPalindromi.java
+++ PalindromoSpringWebMVC/src/main/java/it/dstech/formazione/controller/ControlloPalindromi.java
@@ -1,16 +1,31 @@
 package it.dstech.formazione.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class ControlloPalindromi {
 
 
+//	@RequestMapping(value = "/palindromo", method = RequestMethod.POST)
+//	public String palindromo(@RequestParam("testo") String testo, Model model) {
+//		
+//		GestioneFrase gestione = new GestioneFrase();	
+//		
+//		model.addAttribute("contaParole", gestione.contaParole(testo));
+//		model.addAttribute("contatoreParolePalindrome", gestione.contaParolePalindrome(testo));
+//		model.addAttribute("parolePalindrome", gestione.parolePalindrome);
+//		
+//		return "palindromo";
+//	}
+	
+	
 	@RequestMapping(value = "/palindromo", method = RequestMethod.POST)
-	public String Palindromo(@RequestParam("testo") String testo, Model model) {
+	public ModelAndView palindromo(@RequestParam("testo") String testo, Model model) {
 		
 		GestioneFrase gestione = new GestioneFrase();	
 		
@@ -18,7 +33,7 @@ public class ControlloPalindromi {
 		model.addAttribute("contatoreParolePalindrome", gestione.contaParolePalindrome(testo));
 		model.addAttribute("parolePalindrome", gestione.parolePalindrome);
 		
-		return "palindromo";
+		return new ModelAndView("palindromo");
 	}
 
 }
